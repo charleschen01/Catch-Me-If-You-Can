1fa5a91b43eeb97f3c0200aa5d39d1cb7327ef33
KramPiotr <pk485@cam.ac.uk>
2019-12-06 17:34:32 +0000

    Updated uI
    UI

---
+++ Catch Me If You Can/Assets/Scripts/TimeChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimeChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TextMeshProUGUI player;
+    void Start(){
+
+             player.text = "Score : "; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
