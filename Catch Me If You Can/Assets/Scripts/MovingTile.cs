9bfc1fa869c5743f54ac00243808126df7455896
charleschen <ctx5275@gmail.com>
2019-12-06 18:09:16 +0000

    Added tile

---
+++ Catch Me If You Can/Assets/Scripts/MovingTile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingTile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < 0) {
+            transform.position = new Vector3(-1f,0.25f,-3f);
+        }
+        
+
+    }
+}
