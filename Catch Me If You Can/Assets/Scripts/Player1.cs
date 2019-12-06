34a11dacd491be248336408db5615ff84a088915
charleschen <ctx5275@gmail.com>
2019-12-06 15:30:30 +0000

    Updated

--- Catch Me If You Can/Assets/Scripts/Player1.cs
+++ Catch Me If You Can/Assets/Scripts/Player1.cs
@@ -1,18 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
+ 
 public class Player1 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float _speed = 200f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public Vector3 _velocity = Vector3.forward;
+    public float _deltaAngle = 3f;
+ 
+    void Update(){
+        // if(Input.GetKeyDown(KeyCode.RightArrow)){
+        //     _velocity = Quaternion.Euler(0, 0, _deltaAngle) * _velocity;
+        // }
+        // if(Input.GetKeyDown(KeyCode.LeftArrow)){
+        //     _velocity = Quaternion.Euler(0, 0, -_deltaAngle) * _velocity;
+        // }
+        // if(Input.GetKeyDown(KeyCode.UpArrow)){
+        //     transform.position += _velocity * Time.deltaTime;
+        // }
+        // if(Input.GetKeyDown(KeyCode.DownArrow)){
+        //     transform.position -= _velocity * Time.deltaTime;
+        // }
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            transform.position += _speed * _velocity * Time.deltaTime;
+        }
     }
 }
---
+++ Catch Me If You Can/Assets/Scripts/RotatingBar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingBar : MonoBehaviour
+{
+    [Range(0f, 10f)]
+    public float angle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0.0f,angle,0.0f,Space.Self);
+    }
+}
