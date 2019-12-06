688f4efa1e9794349c53d8ea605b751618df9baf
KramPiotr <pk485@cam.ac.uk>
2019-12-06 15:29:46 +0000

    Players updated
    updated players

--- Catch Me If You Can/Assets/Scripts/Player1.cs
+++ Catch Me If You Can/Assets/Scripts/Player1.cs
@@ -2,7 +2,7 @@
 
 public class Player1 : MonoBehaviour
 {
-    public float thrust = 5.0f;
+    public float thrust = 10.0f;
     public Rigidbody rb;
 
     void Start()
@@ -13,18 +13,16 @@ public class Player1 : MonoBehaviour
     void FixedUpdate()
     {
         if(Input.GetKey(KeyCode.RightArrow)){
-           // Debug.Log("forward "+transform.forward+" right "+transform.right);
             rb.AddForce( Vector3.right * thrust );
         }
         if(Input.GetKey(KeyCode.LeftArrow)){
-            rb.AddForce( - Vector3.right * thrust );
+            rb.AddForce( Vector3.left * thrust );
         }
         if(Input.GetKey(KeyCode.UpArrow)){
-            //Debug.Log("forward "+transform.forward+" right "+transform.right);
             rb.AddForce( Vector3.forward * thrust );
         }
         if(Input.GetKey(KeyCode.DownArrow)){
-            rb.AddForce( - Vector3.forward * thrust);
+            rb.AddForce( Vector3.back * thrust);
         }
         
     }
--- Catch Me If You Can/Assets/Scripts/Player2.cs
+++ Catch Me If You Can/Assets/Scripts/Player2.cs
@@ -2,7 +2,7 @@
 
 public class Player2 : MonoBehaviour
 {
-    public float thrust = 5.0f;
+    public float thrust = 10.0f;
     public Rigidbody rb;
 
     void Start()
@@ -12,19 +12,17 @@ public class Player2 : MonoBehaviour
 
     void FixedUpdate()
     {
-        if(Input.GetKey(KeyCode.RightArrow)){
-           // Debug.Log("forward "+transform.forward+" right "+transform.right);
+        if(Input.GetKey(KeyCode.D)){
             rb.AddForce( Vector3.right * thrust );
         }
-        if(Input.GetKey(KeyCode.LeftArrow)){
-            rb.AddForce( - Vector3.right * thrust );
+        if(Input.GetKey(KeyCode.A)){
+            rb.AddForce( Vector3.left * thrust );
         }
-        if(Input.GetKey(KeyCode.UpArrow)){
-            //Debug.Log("forward "+transform.forward+" right "+transform.right);
+        if(Input.GetKey(KeyCode.W)){
             rb.AddForce( Vector3.forward * thrust );
         }
-        if(Input.GetKey(KeyCode.DownArrow)){
-            rb.AddForce( - Vector3.forward * thrust);
+        if(Input.GetKey(KeyCode.S)){
+            rb.AddForce( Vector3.back * thrust);
         }
         
     }
